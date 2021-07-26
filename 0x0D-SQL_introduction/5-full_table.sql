930797cd6aac97d69b4e4fc1ab12faca804e39a7
aicha709 <aychakhazen3@gmail.com>
2021-07-26 16:06:51 +0100

    SQL - Introduction

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,2 @@
--- Script that prints the full description of the table
--- first_table from the database hbtn_0c_0
-SHOW CREATE TABLE first_table;
+-- show all the information of the table
+SHOW CREATE TABLE first_table
